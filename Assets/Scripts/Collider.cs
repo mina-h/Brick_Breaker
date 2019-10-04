98962a59e36a559fd1c65c46dfb31f3bc7bb6ed7
”Mina” <minaa.hosseinii@gmail.com>
2019-10-04 05:36:57 +0200

    Game basics

---
+++ Assets/Scripts/Collider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collider : MonoBehaviour
+{
+    public Camera cam;
+    public EdgeCollider2D collider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 bottomLeft = cam.ScreenToWorldPoint(new Vector3(0, 0, 0));
+        Vector3 upperLeft = cam.ScreenToWorldPoint(new Vector3(0, Screen.height, 0));
+        Vector3 upperRight = cam.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        Vector3 bottomRight = cam.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0));
+        collider.points = new Vector2[5] { bottomLeft, upperLeft, upperRight, bottomRight, bottomLeft };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float velocity;
+    public float xLimit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform t = GetComponent<Transform>();
+        float mouseXmove = Input.GetAxis("Mouse X"); 
+        t.position = t.position + (Vector3.right * mouseXmove * velocity * Time.deltaTime);
+        float currentX = t.position.x;
+        currentX = Mathf.Clamp(currentX, -xLimit, xLimit);
+        t.position = new Vector3(currentX, t.position.y, t.position.z);  
+        
+    }
+}
